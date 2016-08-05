32c75dc4b6b0048d1d183cf0599bcef0cd7f553a
Marlene Oliveira <marleneoliveira.w@gmail.com>
2016-08-05 23:50:28 +0100

    - Refactored wiki viewer's api call (now it uses opensearch). - Input form now acts as a single block, which includes the headers.

--- fcc-wikipedia-viewer/js/index.js
+++ fcc-wikipedia-viewer/js/index.js
@@ -1,21 +1,25 @@
-/*global $, jQuery, alert*/
+/*global $, jQuery alert*/
+"use strict";
 
 function searchInfo(data) {
-    "use strict";
-    //document.getElementById('test').innerHTML = data.query.search[0].title;
     var ul = document.getElementById("listresults");
-    
-    for (var i = 0; i< data.query.search.length; i++){
+    console.log(data);
+    for (var i = 0; i < data.query.search.length; i++) {
         var li = document.createElement("li");
-        li.innerHTML = "<li><div class=\"boxed\"> <h5> <a href=\"https://en.wikipedia.org/wiki/" + data.query.search[i].title + "\" target = \"_blank\">" + data.query.search[i].title +"</a></h5><p>" + data.query.search[i].snippet + "</p></div></li>";
+        li.innerHTML = "<li><div class=\"boxed\"> <h5> <a href=\"https://en.wikipedia.org/wiki/" + data.query.search[i].title + "\" " +
+            "target = \"_blank\">" + data.query.search[i].title + "</a></h5><p>" + data.query.search[i].snippet + "</p></div></li>";
         ul.appendChild(li);
     }
 }
 
 $("#searchform").submit(function (event) {
-    "use strict";
     $("#listresults").empty();
-    var a = "https://en.wikipedia.org/w/api.php?action=query&limit=10&list=search&srsearch=" + document.getElementById("srch-term").value + "&format=json&callback=?";
-    $.getJSON(a, searchInfo, 'jsonp');
+    //var a = "https://en.wikipedia.org/w/api.php?action=query&exintro&list=search&srsearch=" + document.getElementById("srch-term").value + "&format=json&callback=?";
+    var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + document.getElementById("srch-term").value + "&limit=100&namespace=0&format=json&callback=?";
+    $.getJSON(searchQuery, searchInfo, 'jsonp');
     event.preventDefault();
+});
+
+$('#searchform').on("submit", function () {
+    $('#results').show();
 });
