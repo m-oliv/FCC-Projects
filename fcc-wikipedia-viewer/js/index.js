84b7ec5f679dff7235b74d62f6010cbda32ee1c9
Marlene Oliveira <marleneoliveira.w@gmail.com>
2016-08-10 22:45:52 +0100

    - Fixed bug in search results. They now appear in the interface. - Added CSS to format the list items (they now have a semi-transparent background).

--- fcc-wikipedia-viewer/js/index.js
+++ fcc-wikipedia-viewer/js/index.js
@@ -2,19 +2,20 @@
 "use strict";
 
 function searchInfo(data) {
+    console.log(data[1]);
     var ul = document.getElementById("listresults");
+    var numResults = data[1].length;
     console.log(data);
-    for (var i = 0; i < data.query.search.length; i++) {
+    for (var i = 0; i < numResults; i++) {
         var li = document.createElement("li");
-        li.innerHTML = "<li><div class=\"boxed\"> <h5> <a href=\"https://en.wikipedia.org/wiki/" + data.query.search[i].title + "\" " +
-            "target = \"_blank\">" + data.query.search[i].title + "</a></h5><p>" + data.query.search[i].snippet + "</p></div></li>";
+        li.innerHTML = "<li><div class=\"boxed\"> <h5> <a href=\"" + data[3][i] + "\" " +
+            "target = \"_blank\">" + data[1][i] + "</a></h5><p>" + data[2][i] + "</p></div></li>";
         ul.appendChild(li);
     }
 }
 
 $("#searchform").submit(function (event) {
     $("#listresults").empty();
-    //var a = "https://en.wikipedia.org/w/api.php?action=query&exintro&list=search&srsearch=" + document.getElementById("srch-term").value + "&format=json&callback=?";
     var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + document.getElementById("srch-term").value + "&limit=100&namespace=0&format=json&callback=?";
     $.getJSON(searchQuery, searchInfo, 'jsonp');
     event.preventDefault();
