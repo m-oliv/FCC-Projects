19b45faba89bda9647e06a84021c820350320daf
Marlene Oliveira <marleneoliveira.w@gmail.com>
2016-08-28 15:39:14 +0100

    - The links to the articles now are accessible by clicking the container and not just the title. - Removed underline formatting on hover links. - Removed fixed value formatting (now uses percentages).

--- fcc-wikipedia-viewer/js/index.js
+++ fcc-wikipedia-viewer/js/index.js
@@ -8,15 +8,16 @@ function searchInfo(data) {
     console.log(data);
     for (var i = 0; i < numResults; i++) {
         var li = document.createElement("li");
-        li.innerHTML = "<li><div class=\"boxed\"> <h5> <a href=\"" + data[3][i] + "\" " +
-            "target = \"_blank\">" + data[1][i] + "</a></h5><p>" + data[2][i] + "</p></div></li>";
+        li.innerHTML = "<li><div class=\"boxed\"> <a href=\"" + data[3][i] + "\"+" +
+            "target = \"_blank\"><h5>" + data[1][i] + "</h5><p>" + data[2][i] + "</p></a></div></li>";
         ul.appendChild(li);
     }
 }
 
 $("#searchform").submit(function (event) {
     $("#listresults").empty();
-    var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + document.getElementById("srch-term").value + "&limit=100&namespace=0&format=json&callback=?";
+    var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+        document.getElementById("srch-term").value + "&limit=100&namespace=0&format=json&callback=?";
     $.getJSON(searchQuery, searchInfo, 'jsonp');
     event.preventDefault();
 });
