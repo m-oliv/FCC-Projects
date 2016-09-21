62ce5bf2763f9515878db84f4e85a0d4f5fc09cc
Marlene Oliveira <marleneoliveira.w@gmail.com>
2016-09-21 22:18:35 +0100

    - Added floating button that allows the user to return to the top of the page.

--- fcc-wikipedia-viewer/js/index.js
+++ fcc-wikipedia-viewer/js/index.js
@@ -25,3 +25,21 @@ $("#searchform").submit(function (event) {
 $('#searchform').on("submit", function () {
     $('#results').show();
 });
+
+$(document).ready(function () {
+
+    var offset = 250;
+    var duration = 300;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+    $('.back-to-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
