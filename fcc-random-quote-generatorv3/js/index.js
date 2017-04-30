61d80ae9f929ebb603b28cbb287cefaa399ae12d
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-04-30 22:38:01 +0100

    - Changed the quote API in order to use a source with HTTPS. - Refactored the request function (created getQuoteDataRequest). - Refactored the getQuoteAndAuthor input. - Added comments.

--- fcc-random-quote-generatorv3/js/index.js
+++ fcc-random-quote-generatorv3/js/index.js
@@ -9,33 +9,48 @@ var text = "";
 var author = "";
 
 // get the quote and the author
-function getQuoteAndAuthor (data) {
+function getQuoteAndAuthor(dataString) {
+    var data = JSON.parse(dataString);
+
     // pass the info to the corresponding html
-    $(".text").text(data.quoteText);
-    $(".author").text(data.quoteAuthor);
-    
+    $(".text").text(data.quote);
+    $(".author").text(data.author);
+
     // prevent cases in which there's no author
-    if (data.quoteAuthor === "") {
+    if (data.author === "") {
         $(".author").text("Anonymous");
     }
-    
+
     // save info to tweet in vars (to be used in other contexts)
-    text = data.quoteText;
-    author = data.quoteAuthor;
+    text = data.quote;
+    author = data.author;
 };
+
+// Request the quote data(author and quote text) from the API.
+function getQuoteDataRequest() {
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+        success: getQuoteAndAuthor
+    });
+}
 
 // get a quote when the page loads
 $(document).ready(function () {
-    $.getJSON(quoteURL, getQuoteAndAuthor, 'jsonp');
+    getQuoteDataRequest();
 });
 
 // get next quote when button is clicked
 $("#nextQ").click(function () {
-    $.getJSON(quoteURL, getQuoteAndAuthor, 'jsonp');
+    getQuoteDataRequest();
 });
 
 // tweet quote when button is clicked
 $("#tweetlink").click(function () {
     // get quote string (as per twitter's guidelines) and pass it as href
-    $("#tweetlink").attr("href", 'https://twitter.com/intent/tweet?text=' + text  + ' - ' + author);
+    $("#tweetlink").attr("href", 'https://twitter.com/intent/tweet?text=' + text + ' - ' + author);
 });
