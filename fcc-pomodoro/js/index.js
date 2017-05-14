70a716c141edd62a56664336f192307dab3a9ce4
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-05-14 23:15:17 +0100

    - Changed the background to white for debug. - Added the basic HTML elements. - Added structure that saves the user inputs.

--- fcc-pomodoro/js/index.js
+++ fcc-pomodoro/js/index.js
@@ -2,5 +2,6 @@
 
 angular.module('fccPomodoro', ['ngMaterial'])
     .controller('PomodoroDashboardController', function ($scope, $log, $q, $timeout, $window) {
-            
+         $scope.time = { break: 0, work: 0};
+         
     });
