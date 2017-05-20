49b1f2b68c85a8f39a8814329d11ffc3ce71e7a8
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-05-20 23:36:24 +0100

    - Added HTML placeholders to show the work timer and break timer. - Added work and break timer variables.

--- fcc-pomodoro/js/index.js
+++ fcc-pomodoro/js/index.js
@@ -3,5 +3,13 @@
 angular.module('fccPomodoro', ['ngMaterial'])
     .controller('PomodoroDashboardController', function ($scope, $log, $q, $timeout, $window) {
          $scope.time = { break: 0, work: 0};
-         
+         $scope.timerBreak = 0;
+         $scope.timerWork = 0;
+
+         /**
+          * TODO: count the time
+          see this:
+            - https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+            - https://stackoverflow.com/questions/12050268/angularjs-make-a-simple-countdown
+          */
     });
